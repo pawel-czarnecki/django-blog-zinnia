503c5956992e7cca113580c1e7f38c71fdd24462
Paweł Czarnecki <pawel.czarnecki@halodoktorze.pl>
2017-07-19 17:38:06 +0200

    Fixed pagination

--- zinnia/templatetags/zinnia_tags.py
+++ zinnia/templatetags/zinnia_tags.py
@@ -248,25 +248,21 @@ def get_recent_linkbacks(number=5,
 def zinnia_pagination(context, page, begin_pages=3, end_pages=3,
                       before_pages=2, after_pages=2,
                       template='zinnia/tags/pagination.html'):
-    """Return a Digg-like pagination, by splitting long list of page
-    into 3 blocks of pages"""
+    """
+    Return a Digg-like pagination,
+    by splitting long list of page into 3 blocks of pages.
+    """
     GET_string = ''
     for key, value in context['request'].GET.items():
         if key != 'page':
             GET_string += '&%s=%s' % (key, value)
 
-    begin = page.paginator.page_range[:begin_pages]
-    end = page.paginator.page_range[-end_pages:]
-    middle = page.paginator.page_range[max(page.number - before_pages - 1, 0):
-                                       page.number + after_pages]
+    begin = list(page.paginator.page_range[:begin_pages])
+    end = list(page.paginator.page_range[-end_pages:])
+    middle = list(page.paginator.page_range[
+        max(page.number - before_pages - 1, 0):page.number + after_pages])
 
-    if set(begin) & set(end):  # [1, 2, 3], [...], [2, 3, 4]
-        begin = sorted(set(begin + end))  # [1, 2, 3, 4]
-        middle, end = [], []
-    elif begin[-1] + 1 == end[0]:  # [1, 2, 3], [...], [4, 5, 6]
-        begin += end  # [1, 2, 3, 4, 5, 6]
-        middle, end = [], []
-    elif set(begin) & set(middle):  # [1, 2, 3], [2, 3, 4], [...]
+    if set(begin) & set(middle):  # [1, 2, 3], [2, 3, 4], [...]
         begin = sorted(set(begin + middle))  # [1, 2, 3, 4]
         middle = []
     elif begin[-1] + 1 == middle[0]:  # [1, 2, 3], [4, 5, 6], [...]
@@ -278,6 +274,13 @@ def zinnia_pagination(context, page, begin_pages=3, end_pages=3,
     elif set(middle) & set(end):  # [...], [17, 18, 19], [18, 19, 20]
         end = sorted(set(middle + end))  # [17, 18, 19, 20]
         middle = []
+
+    if set(begin) & set(end):  # [1, 2, 3], [...], [2, 3, 4]
+        begin = sorted(set(begin + end))  # [1, 2, 3, 4]
+        middle, end = [], []
+    elif begin[-1] + 1 == end[0]:  # [1, 2, 3], [...], [4, 5, 6]
+        begin += end  # [1, 2, 3, 4, 5, 6]
+        middle, end = [], []
 
     return {'template': template, 'page': page, 'GET_string': GET_string,
             'begin': begin, 'middle': middle, 'end': end}
